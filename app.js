866789d7aa0c6bd9589ed783a86adb8aa27c52df
Mohamad Khan <mohamadkhan19@gmail.com>
2017-04-12 15:51:16 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var userRoutes = require('./routes/user');
+var formRoutes = require('./routes/form');
+var sensor_dataRoutes = require('./routes/sensor_data');
+var led_dataRoutes = require('./routes/led_data');
+
+
+var app = express();
+mongoose.connect('mongodb://root:toor@ds157819.mlab.com:57819/restapi_deployement');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/users', userRoutes);
+app.use('/form', formRoutes);
+app.use('/sensor_data', sensor_dataRoutes);
+app.use('/led_data', led_dataRoutes);
+
+
+module.exports = app;
