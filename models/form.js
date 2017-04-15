bd8ace49863233d41c7a0adf70ab5b049a4e0515
Mohamad Khan <mohamadkhan19@gmail.com>
2017-04-15 14:14:31 -0700

    updated form details

--- models/form.js
+++ models/form.js
@@ -9,8 +9,7 @@ var schema = new Schema({
     city: {type: String, required: true},
     state: {type: String, required: true},
     zip: {type: Number, required: true},
-    phone: {type: String, required: true},
-    user_id: {type: Schema.Types.ObjectId, ref: 'User_id'}
+    phone: {type: String, required: true}
 });
 
 schema.post('remove', function (form) {
