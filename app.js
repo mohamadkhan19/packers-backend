b9a588563ec2f29169fec1eb222c4d2a9a9b15a0
Mohamad Khan <mohamadkhan19@gmail.com>
2017-04-29 18:39:47 -0700

    changes mades

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var userRoutes = require('./routes/user');
 var formRoutes = require('./routes/form');
 var sensor_dataRoutes = require('./routes/sensor_data');
 var led_dataRoutes = require('./routes/led_data');
+var notification = require('./routes/notification');
 var latest_sensor_data = require('./routes/latest_sensor_data');
 
 
@@ -20,6 +21,7 @@ app.use('/form', formRoutes);
 app.use('/sensor_data', sensor_dataRoutes);
 app.use('/latest_sensor_data', latest_sensor_data);
 app.use('/led_data', led_dataRoutes);
+app.use('/notification', notification);
 
 
 module.exports = app;
